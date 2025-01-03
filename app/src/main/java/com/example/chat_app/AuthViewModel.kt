b61c564add503a1751591931461a85b9baecbe16
ElDavido81 <eldavido81@gmail.com>
2025-01-03 12:10:58 +0100

    dk-login_start

---
+++ app/src/main/java/com/example/chat_app/AuthViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.chat_app
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class AuthViewModel {
+
+    val auth: FirebaseAuth = Firebase.auth
+
+
+
+    fun Login(etEmail : String, etPassword : String) : Boolean {
+
+        var status : Boolean = false
+
+        auth.signInWithEmailAndPassword(etEmail, etPassword).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                status = true
+            } else {
+                status = false
+
+            }
+        }
+        return status
+    }
+
+}
--- app/src/main/java/com/example/chat_app/LoginFragment.kt
+++ app/src/main/java/com/example/chat_app/LoginFragment.kt
@@ -1,17 +1,51 @@
 package com.example.chat_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
+
+    private lateinit var etEmail: TextInputEditText
+    private lateinit var etPassword: TextInputEditText
+
+    private val authViewModel = AuthViewModel()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+            view.findViewById<Button>(R.id.btn_auth_login).setOnClickListener {
+            etEmail = view.findViewById(R.id.et_login_email)
+            etPassword = view.findViewById(R.id.et_login_password)
+            login(etEmail.text.toString(), etPassword.text.toString())
+        }
+
+    }
+
+    fun login(etEmail: String, etPassword: String){
+        val result = authViewModel.Login(etEmail, etPassword)
+
+        if (result){
+            Log.d("!!!", "Succesfully signed in!")
+        } else {
+            Log.d("!!!", "Not signed in!")
+        }
+
     }
 }
